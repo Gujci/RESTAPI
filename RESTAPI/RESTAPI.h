623b064cd77022c4f5b3210276eb4b32cd8ddc6a
Mate Gujgiczer <mate.gujgiczer@icloud.com>
2016-07-22 18:22:32 +0200

    - initial commit

---
+++ RESTAPI/RESTAPI.h
@@ -0,0 +1,19 @@
+//
+//  RESTAPI.h
+//  RESTAPI
+//
+//  Created by Gujgiczer Máté on 22/07/16.
+//  Copyright © 2016 gujci. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RESTAPI.
+FOUNDATION_EXPORT double RESTAPIVersionNumber;
+
+//! Project version string for RESTAPI.
+FOUNDATION_EXPORT const unsigned char RESTAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RESTAPI/PublicHeader.h>
+
+
