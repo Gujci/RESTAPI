92ca70f87c800a18629a073801f3fcc630db1700
Mate Gujgiczer <mate.gujgiczer@icloud.com>
2018-11-07 11:19:42 +0100

    - Query test fix for non deterministic query parse order

--- RESTAPITests/RESTAPIComponentTests.swift
+++ RESTAPITests/RESTAPIComponentTests.swift
@@ -14,15 +14,15 @@ class RESTAPIComponentTests: XCTestCase {
 
     func testComplexQuery() {
         let testDict = ["level1_0": "key1", "level1_1": ["array1", "array2"]] as [String : Any]
-        let desiredQueryString = "&test[level1_0]=key1&test[level1_1][]=array1&test[level1_1][]=array2"
-        let query = testDict.queryString(forKey: "test").reduce("") { acc, it in acc + "&\(it.name)=\(it.value ?? "")"}
+        let desiredQueryString = ["test[level1_0]=key1", "test[level1_1][]=array1", "test[level1_1][]=array2"].sorted()
+        let query = testDict.queryString(forKey: "test").map { it in "\(it.name)=\(it.value ?? "")"}.sorted()
         XCTAssertEqual(desiredQueryString, query)
     }
     
     func testDeepComplexQuery() {
         let testDict = ["level_0": ["level1_0": "key1", "level1_1": ["array1", "array2"]]] as [String : Any]
-        let desiredQueryString = "&test[level_0][level1_0]=key1&test[level_0][level1_1][]=array1&test[level_0][level1_1][]=array2"
-        let query = testDict.queryString(forKey: "test").reduce("") { acc, it in acc + "&\(it.name)=\(it.value ?? "")"}
+        let desiredQueryString = ["test[level_0][level1_0]=key1", "test[level_0][level1_1][]=array1", "test[level_0][level1_1][]=array2"]
+        let query = testDict.queryString(forKey: "test").map { it in "\(it.name)=\(it.value ?? "")"}.sorted()
         XCTAssertEqual(desiredQueryString, query)
     }
 }
