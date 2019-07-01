cc261922d4186969cbdbd95f490bbb04ed8fbcdc
Máté Gujgiczer <mate.gujgiczer@icloud.com>
2019-07-01 16:28:58 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,13 +4,13 @@ import PackageDescription
 let package = Package(
     name: "RESTAPI",
     products: [
-        .library(name: "RESTAPI", targets: [.target(name: "RESTAPI",path: ".")]),
+        .library(name: "RESTAPI", targets: ["RESTAPI"]),
     ],
     dependencies: [
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.0")
     ],
     targets: [
-        .target(name: "RESTAPI", dependencies: ["SwiftyJSON"]),
-        .testTarget(name: "RESTAPITests", dependencies: ["RESTAPI"]),
+        .target(name: "RESTAPI", dependencies: ["SwiftyJSON"], path: "./RESTAPI"),
+        .testTarget(name: "RESTAPITests", dependencies: ["RESTAPI"], path: "./RESTAPITests"),
     ]
 )
