84fbd5a10990e79f9d23353c0ffa74938f5f96b5
Máté Gujgiczer <mate.gujgiczer@icloud.com>
2019-07-15 16:19:03 +0200

    - Cacheable protocol added - related UIImage extensions provided

--- Package.swift
+++ Package.swift
@@ -3,8 +3,12 @@ import PackageDescription
 
 let package = Package(
     name: "RESTAPI",
+    platforms: [
+        .macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v3)
+    ],
     products: [
         .library(name: "RESTAPI", targets: ["RESTAPI"]),
+        .library(name: "RESTAPIImage", targets: ["RESTAPIImage"])
     ],
     dependencies: [
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.0")
@@ -12,5 +16,7 @@ let package = Package(
     targets: [
         .target(name: "RESTAPI", dependencies: ["SwiftyJSON"], path: "RESTAPI"),
         .testTarget(name: "RESTAPITests", dependencies: ["RESTAPI"], path: "RESTAPITests"),
+        .target(name: "RESTAPIImage", dependencies: ["RESTAPI"], path: "RESTAPIImage"),
+        .testTarget(name: "RESTAPIImageTests", dependencies: ["RESTAPIImage"], path: "RESTAPIImageTests")
     ]
 )
---
+++ RESTAPIImage/UIImage+Cachable.swift
@@ -0,0 +1,59 @@
+//
+//  File.swift
+//  
+//
+//  Created by Gujgiczer Mate on 2019. 07. 08..
+//
+
+import RESTAPI
+#if canImport(UIKit)
+import UIKit
+
+// MARK: - UIImage conformance
+extension UIImage: ValidResponseData {
+    
+    public static func createInstance(from data: Data) throws -> Self { return UIImage(data: data)! as! Self }
+}
+
+extension UIImage: Cachable {
+    
+    private static var cacheURL: URL {
+        return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+    }
+    
+    public func savePersistant(for url: String) throws {
+        let cacheUrl = UIImage.cacheURL.appendingPathComponent(url.components(separatedBy: "/").last!)
+        
+        try pngData()?.write(to: cacheUrl, options: .atomicWrite)
+    }
+    
+    public static func getPersistantData(for url: String) throws -> Self? {
+        let cacheUrl = cacheURL.appendingPathComponent(url.components(separatedBy: "/").last!)
+        let data = try Data(contentsOf: cacheUrl, options: .mappedIfSafe)
+        return UIImage(data: data) as? Self
+    }
+}
+
+// MARK: UIImageView extension
+public extension UIImageView {
+    
+    private static let loader = API(withBaseUrl: "")
+    
+    private struct Holder {
+        static var _imageURL: String?
+    }
+    
+    var imageUrl: String? {
+        get {
+            return Holder._imageURL
+        }
+        set {
+            Holder._imageURL = newValue
+            guard let url = newValue else { return }
+            UIImageView.loader.load(from: url, cachePolicy: .refreshCache) { [weak self] (image) in
+                DispatchQueue.main.async { self?.image = image }
+            }
+        }
+    }
+}
+#endif
